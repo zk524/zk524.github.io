347ca154c7b3f557b401be8c9cb7d81b32e55668
zk524 <20262010007@fudan.edu.cn>
2022-09-02 18:33:53 +0800

    rank

--- index.js
+++ index.js
@@ -4,25 +4,57 @@
 // const fetch = require("node-fetch");
 
 (async function () {
-  const data = await fetch(
-    "https://evm-golf.github.io/index/addition/gasrank.json"
-  )
-    .then((data) => data.json())
-    .catch(() => []);
-  console.log(data);
+  const [gasrank, lenrank, gasbest, lenbest] = await Promise.all([
+    fetch("https://evm-golf.github.io/index/addition/gasrank.json")
+      .then((data) => data.json())
+      .catch(() => []),
+    fetch("https://evm-golf.github.io/index/addition/lenrank.json")
+      .then((data) => data.json())
+      .catch(() => []),
+    fetch("https://evm-golf.github.io/index/addition/gasbest.json")
+      .then((data) => data.json())
+      .catch(() => []),
+    fetch("https://evm-golf.github.io/index/addition/lenbest.json")
+      .then((data) => data.json())
+      .catch(() => []),
+  ]);
+  console.log(gasrank, lenrank, gasbest, lenbest);
+
   const table = document.getElementById("table");
   const fragment = document.createDocumentFragment();
-  data.forEach((d, index) => {
+  const len = [gasrank, lenrank, gasbest, lenbest].reduce(
+    (res, cur) => Math.max(cur.length, res),
+    0
+  );
+  new Array(len).fill().forEach((_, index) => {
     const tr = document.createElement("tr");
-    const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
-    const td3 = document.createElement("td");
-    td1.innerText = index + 1;
-    td2.innerText = `${d.user}: ${d.gas}`;
-    td3.innerText = `${d.user}: ${d.length}`;
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
+    const _id = document.createElement("td");
+    const _gasrank = document.createElement("td");
+    const _lenrank = document.createElement("td");
+    const _gasbest = document.createElement("td");
+    const _lenbest = document.createElement("td");
+    _id.innerText = index + 1;
+    _gasrank.innerText =
+      gasrank[index] === void 0
+        ? ""
+        : `${gasrank[index].user} : ${gasrank[index].gas} : ${gasrank[index].length}`;
+    _lenrank.innerText =
+      lenrank[index] === void 0
+        ? ""
+        : `${lenrank[index].user} : ${lenrank[index].gas} : ${lenrank[index].length}`;
+    _gasbest.innerText =
+      gasbest[index] === void 0
+        ? ""
+        : `${gasbest[index].user} : ${gasbest[index].gas} : ${gasbest[index].length}`;
+    _lenbest.innerText =
+      lenbest[index] === void 0
+        ? ""
+        : `${lenbest[index].user} : ${lenbest[index].gas} : ${lenbest[index].length}`;
+    tr.appendChild(_id);
+    tr.appendChild(_gasrank);
+    tr.appendChild(_lenrank);
+    tr.appendChild(_gasbest);
+    tr.appendChild(_lenbest);
     fragment.appendChild(tr);
   });
   table.appendChild(fragment);
--- index.md
+++ index.md
@@ -1,8 +1,10 @@
 <div id="app"><table id="table">
     <tr>
-      <th>No</th>
-      <th>gas</th>
-      <th>length</th>
+      <th>NO</th>
+      <th>gasrank</th>
+      <th>gasbest</th>
+      <th>lenrank</th>
+      <th>lenbest</th>
     </tr>
   </table></div>
 <script src="https://zk524.github.io/index.js"></script>
