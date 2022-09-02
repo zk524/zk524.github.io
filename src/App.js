4c467c3733714f167adc461a6b335920a4079a71
zk524 <20262010007@fudan.edu.cn>
2022-09-02 11:47:33 +0800

    octokit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import LoginGithub from "react-login-github";
+import { Octokit } from "octokit";
+import { createOAuthDeviceAuth } from "@octokit/auth-oauth-device";
 
-const onSuccess = (response) => console.log(response);
-const onFailure = (response) => console.error(response);
+const auth = async () => {
+  const octokit = new Octokit({
+    authStrategy: createOAuthDeviceAuth,
+    auth: {
+      clientId: "c70e3f7ef445b54312ba",
+      scopes: [`public_repo`],
+      onVerification(verification) {
+        console.error(
+          `please visit the following link in your browser (login your github account if required):`
+        );
+        console.error();
+        console.error(`${verification.verification_uri}`);
+        console.error();
+        console.error(`and then enter the following code:`);
+        console.error();
+        console.error(`${verification.user_code}`);
+        console.error();
+      },
+    },
+  });
+  const auth = await octokit.auth({ type: "oauth" });
+  console.error(`export GITHUB_AUTH_TOKEN=${auth.token}`);
+};
 
 function App() {
   return (
     <div className="App">
-      <LoginGithub
-        clientId="Iv1.f0decd051fd9c5aa"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-      />
+      <button
+        onClick={() => {
+          // console.log("test");
+          auth();
+        }}
+      >
+        Test
+      </button>
     </div>
   );
 }
